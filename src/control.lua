43d3c34fefff1480dde3db94f268038478779d07
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2020-05-09 14:43:34 +0200

    Fixed crash when there were no player's character (like in sandbox mode).

--- src/control.lua
+++ src/control.lua
@@ -1,4 +1,6 @@
 script.on_event(defines.events.on_player_respawned, function(event)
   local player = game.players[event.player_index]
-  player.character.clear_items_inside()
+  if player.character then
+    player.character.clear_items_inside()
+  end
 end)
