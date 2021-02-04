0ce255b6ca571475842c46243557c0baa8f06ed3
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2021-02-04 19:26:21 +0100

    Used the proper method to clear the player's inventory which should be much more reliable and shoudn't affect game modes other than freeplay anymore.

--- src/control.lua
+++ src/control.lua
@@ -1,6 +1,5 @@
-script.on_event(defines.events.on_player_respawned, function(event)
-  local player = game.players[event.player_index]
-  if player.character then
-    player.character.clear_items_inside()
-  end
+script.on_event(defines.events.on_game_created_from_scenario, function(event)
+	if remote.interfaces.freeplay and remote.interfaces.freeplay.set_respawn_items then
+		remote.call("freeplay", "set_respawn_items", {})
+	end
 end)
